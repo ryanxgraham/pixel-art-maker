32cb148947fe0fa63404894113691099e0da216c
ryanxgraham <59671937+ryanxgraham@users.noreply.github.com>
2020-04-03 15:24:18 -0500

    Cleaned up a little bit

--- designs.js
+++ designs.js
@@ -1,15 +1,18 @@
 
 const sizePicker = document.getElementById('sizePicker');
+const canvas = document.getElementById('pixelCanvas')
 
 sizePicker.addEventListener('submit', makeGrid, true);
+canvas.addEventListener('click', colorizer, true);
 
+
+//Makes a grid
 function makeGrid(event) {
     // Prevents refresh
     event.preventDefault();
     // Define Variables
     const height = document.getElementById('inputHeight').value;
     const width = document.getElementById('inputWidth').value;
-    const canvas = document.getElementById('pixelCanvas')
     // Clean the Canvas
     canvas.innerHTML=null;
     //Create the Canvas
@@ -20,22 +23,20 @@ function makeGrid(event) {
             cell.style.backgroundColor = 'white';
         }
     }
-    //Adds Color
-    function colorizer(event) {
-        var color = document.getElementById('colorPicker').value;
-            //Checks if target is valid candidate and it's current color
-            //if the color is the same, change to white, else change to color
-            if (event.target.nodeName === 'TD') {
-                if (event.target.currentColor === color) {
-                        event.target.style.backgroundColor = 'white';
-                        event.target.currentColor = 'white';
-                }
-                else {
-                    event.target.style.backgroundColor = color;
-                    event.target.currentColor = color;
-                }
-
-            }
+}
+//Adds Color
+function colorizer(event) {
+    var color = document.getElementById('colorPicker').value;
+    //Checks if target is valid candidate and it's current color
+    //if the color is the same, change to white, else change to color
+    if (event.target.nodeName === 'TD') {
+        if (event.target.currentColor === color) {
+            event.target.style.backgroundColor = 'white';
+            event.target.currentColor = 'white';
+        }
+        else {
+            event.target.style.backgroundColor = color;
+            event.target.currentColor = color;
+        }
     }
-    canvas.addEventListener('click', colorizer);
 }
